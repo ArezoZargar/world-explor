010d0e69a9963613eefddb9bd9113bb717a762b9
ArezoZargar <arezozargar8@gmail.com>
2026-07-22 13:59:56 +0430

    Changes

--- app/search/page.js
+++ app/search/page.js
@@ -2,6 +2,7 @@ import { getCountries } from "@/lib/countries";
 import CountrySearch from "@/components/CountrySearch";
 
 export default async function SearchPage() {
+
   const countries = await getCountries();
 
   return (
